575f321c7f9540f36304ffc97342d517f77fe765
Joao Breno Gomes <138957799+Jhonnee101@users.noreply.github.com>
2023-10-06 11:32:56 -0300

    att

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:todo/pages/home_page.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  //Inicia o hive
+  await Hive.initFlutter();
+
+  var box = await Hive.openBox("mybox");
+
   runApp(const Myapp());
 }
 
